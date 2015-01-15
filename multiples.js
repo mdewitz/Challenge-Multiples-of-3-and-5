248d8ecad407ed0276f58eb22ddd0c46fcddd881
Mika <mikadewitz@gmail.com>
2015-01-14 19:28:47 -1000

    Solved multiple og 3 and 5 challenge with for loops

--- multiples.js
+++ multiples.js
@@ -5,7 +5,12 @@
  */
 exports.sumOfAMultiple = function(n){
   var sum = 0;
-  // do your work here
-  
+  for (var i = 2; i < n; i++){
+    if ((i % 3) === 0 || (i % 5) === 0) {
+     
+      sum += i;
+    }
+  } 
+  console.log(sum);
   return sum;
 };
